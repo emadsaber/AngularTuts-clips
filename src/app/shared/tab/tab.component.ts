25f5ae648a27cbc800d37d5b6d5f7f31e8f8e9cb
Emad Saber <a.ehamed@dev.thiqah.sa>
2022-09-14 16:08:43 +0200

    tabs component

--- src/app/shared/tab/tab.component.ts
+++ src/app/shared/tab/tab.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab',
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tab.component.css']
 })
 export class TabComponent implements OnInit {
-
+  @Input() title = '';
+  @Input() active = false;
+  
   constructor() { }
 
   ngOnInit(): void {
--- src/app/shared/tabs-container/tabs-container.component.ts
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentInit, Component, ContentChildren, OnInit, QueryList } from '@angular/core';
+import { TabComponent } from '../tab/tab.component';
 
 @Component({
   selector: 'app-tabs-container',
   templateUrl: './tabs-container.component.html',
   styleUrls: ['./tabs-container.component.css']
 })
-export class TabsContainerComponent implements OnInit {
+export class TabsContainerComponent implements OnInit, AfterContentInit {
+
+  @ContentChildren(TabComponent) tabs : QueryList<TabComponent> = new QueryList<TabComponent>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterContentInit(): void {
+      const activeTabs = this.tabs?.filter(t => t.active)
+
+      if(!activeTabs || activeTabs.length === 0){
+        this.selectTab(this.tabs!.first);
+      }
+  } 
+
+  selectTab(tab: TabComponent){
+    this.tabs.forEach(t => t.active = false);
+
+    tab.active = true;
   }
 
 }
