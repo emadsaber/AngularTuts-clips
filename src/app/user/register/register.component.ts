d0fa902916b7a4713481db64db9d249ec14e74c3
emadsaber <omda.sharkawy@gmail.com>
2022-09-28 21:28:29 +0200

    new commit

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -7,15 +7,46 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  name = new FormControl('', [
+    Validators.required,
+    Validators.minLength(3)
+  ]);
+  email = new FormControl('', [
+    Validators.required,
+    Validators.email
+  ]);
+  age = new FormControl('', [
+    Validators.min(15),
+    Validators.max(70)
+  ]);
+  password =  new FormControl('', [
+    Validators.required,
+    Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm)
+  ]);
+  confirm_password = new FormControl('', [
+    Validators.required,
+    Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm)
+  ]);
+  phoneNumber = new FormControl('', [
+    Validators.required,
+    Validators.minLength(14),
+    Validators.maxLength(14),
+  ]);
+
   registerForm: FormGroup = new FormGroup({
-    name: new FormControl('', [
-      Validators.required,
-      Validators.minLength(3)
-    ]),
-    email: new FormControl(''),
-    age: new FormControl(''),
-    password: new FormControl(''),
-    confirm_password: new FormControl(''),
-    phoneNumber: new FormControl('')
+    name: this.name,
+    email: this.email,
+    age: this.age,
+    password: this.password,
+    confirm_password: this.password,
+    phoneNumber: this.confirm_password
   })
+
+  alertColor = 'blue';
+  alertMessage = 'Account Created!';
+  showAlert = false;
+
+  save(){
+    this.showAlert = true;
+  }
 }
