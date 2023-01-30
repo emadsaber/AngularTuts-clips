4e620fc2ddf931841e808ec56b749b2652fcf2d4
Emad Saber Hamed <omda.sharkawy@live.com>
2023-01-30 23:24:33 +0200

    Firebase : login

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,23 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  alertColor = 'blue';
+  showAlert = false;
+  alertMsg = '';
+  isSubmission = false;
 
   credentials = {
     email: '',
-    password: ''
-  }
+    password: '',
+  };
 
-  constructor() { }
+  constructor(private auth: AngularFireAuth) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  login(){
+  async login() {
+    this.isSubmission = true;
+    this.alertMsg = 'Please wait....';
+    this.showAlert = true;
+    try {
+      await this.auth.signInWithEmailAndPassword(
+        this.credentials.email,
+        this.credentials.password
+      );
+      this.showAlert = false;
+    } catch (error) {
+      this.isSubmission = false;
+      this.alertMsg = 'Failed to login!';
+      this.alertColor = 'red';
+      this.showAlert = true;
+    }
+
     console.log(this.credentials);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,19 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import firebase from 'firebase/compat/app'
 
 if (environment.production) {
   enableProdMode();
 }
+let appInit = false;
+firebase.initializeApp(environment.firebase);
+firebase.auth().onAuthStateChanged(() => {
+  if(!appInit){
+    platformBrowserDynamic().bootstrapModule(AppModule)
+    .catch(err => console.error(err));
+  }
+  appInit = true;
+});
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+
